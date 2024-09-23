80e7987b01fd51e2e4d35902ee3cd5f98cb47ff1
김선인 <suninkim10@gmail.com>
2024-09-24 00:37:01 +0900

    feat:기본설정 보기, 변경 기능추가 완료

--- src/renderer/components/AdminSetting.tsx
+++ src/renderer/components/AdminSetting.tsx
@@ -1,15 +1,24 @@
 import "../styles/AdminSetting.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDialog } from "../hooks/useDialog";
 
 function AdminSetting(){
   const {alert} = useDialog();
 
   const [basicTime, setBasicTime] = useState({
-    checkInTime: 2,
+    checkInTime: 1,
     extendTime: 1,
     askCheckOutTime: 1,
   });
+
+  useEffect(() => {
+    viewConfig();
+  }, []);
+
+  const viewConfig = async ()=>{
+    let res = await window.electron.viewConfig();
+    setBasicTime({checkInTime: res.data.reservation_time, extendTime: res.data.extend_time, askCheckOutTime: res.data.ask_checkout_time})
+  }
 
   const onChange = (e:any) => {
     if(e.target.value >= 1 && e.target.value <= 3) {
@@ -17,54 +26,54 @@ function AdminSetting(){
     }
   }
 
-  const onSubmit = async ()=>{
-    await alert("기본설정", "시간설정이 적용되었습니다.")
-  }
+  const onSubmit = async () => {
+    let res = await window.electron.updateConfig(
+      basicTime.checkInTime,
+      basicTime.extendTime,
+      basicTime.askCheckOutTime,
+    );
+    if (res.code == 200) {
+      await alert('기본설정', '기본설정이 정상적으로 변경되었습니다.');
+      await viewConfig();
+    } else {
+      await alert('오류', '기본설정 변경에 오류가 발생했습니다.');
+    }
+  };
 
 
   return (
     <div className={'admin-setting'}>
-        <div>
-          <label>기본 예약시간 변경</label>
-          <input
-            onChange={onChange}
-            name={'checkInTime'}
-            value={basicTime.checkInTime}
-            type={'number'}
-          />
-        </div>
-        <div>
-          <label>기본 연장시간 변경</label>
-          <input
-            onChange={onChange}
-            name={'extendTime'}
-            value={basicTime.extendTime}
-            type={'number'}
-          />
-        </div>
-        <div>
-          <label>퇴실요청 대기시간 변경</label>
-          <input
-            onChange={onChange}
-            name={'askCheckOutTime'}
-            value={basicTime.askCheckOutTime}
-            type={'number'}
-          />
-        </div>
-        <div>
-          <button
-            onClick={() =>
-              setBasicTime({
-                checkInTime: 2,
-                extendTime: 1,
-                askCheckOutTime: 1,
-              })
-            }
-          >
-            초기화
-          </button>
-          <button onClick={onSubmit}>적용</button>
-        </div>
+      <div>
+        <label>기본 예약시간 변경</label>
+        <input
+          onChange={onChange}
+          name={'checkInTime'}
+          value={basicTime.checkInTime}
+          type={'number'}
+        />
+      </div>
+      <div>
+        <label>기본 연장시간 변경</label>
+        <input
+          onChange={onChange}
+          name={'extendTime'}
+          value={ basicTime.extendTime}
+          type={'number'}
+        />
+      </div>
+      <div>
+        <label>퇴실요청 대기시간 변경</label>
+        <input
+          onChange={onChange}
+          name={'askCheckOutTime'}
+          value={basicTime.askCheckOutTime}
+          type={'number'}
+        />
+      </div>
+      <div>
+        <button onClick={viewConfig}>초기화</button>
+        <button onClick={onSubmit}>적용</button>
+      </div>
     </div>
   );
 }
